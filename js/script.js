b772b30e660fb0b033a9240a2475687119e553aa
Karan <karan.2005iitjee2023@gmail.com>
2025-12-27 23:31:30 +0530

    Implement mobile menu toggle and email button
    Added mobile menu toggle functionality and email sending feature on button click.

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ window.addEventListener('scroll', () => {
 const toggleTheme = () => {
   document.body.classList.toggle('light-mode');
 };
+
+// Mobile menu toggle
+    const toggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+// On Hire me button click send email
+const hireMeBtn = document.getElementById("hireMeBtn");
+
+  hireMeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const email = "karan.singh022005@gmail.com";
+    const subject = "Hiring Inquiry – Portfolio Website";
+    const body =
+      "Hi Karan,%0D%0A%0D%0A" +
+      "I visited your portfolio and would like to work with you.%0D%0A%0D%0A" +
+      "Project Details:%0D%0A" +
+      "- Type:%0D%0A" +
+      "- Budget:%0D%0A" +
+      "- Timeline:%0D%0A%0D%0A" +
+      "Regards,%0D%0A" +
+      "Your Name";
+
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  });
